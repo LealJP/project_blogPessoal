428e9fd6814ac78bba3967c648cf3757d6c1e8a3
unknown <juportoleal@gmail.com>
2021-06-11 16:33:12 -0300

    Feat. Implementando Junit (bem no inicio)

--- blogPessoal/src/main/java/org/generation/blogPessoal/repository/UsuarioRepository.java
+++ blogPessoal/src/main/java/org/generation/blogPessoal/repository/UsuarioRepository.java
@@ -4,7 +4,9 @@ import java.util.Optional;
 
 import org.generation.blogPessoal.model.Usuario;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
 
+@Repository
 public interface UsuarioRepository extends JpaRepository<Usuario, Long> {
 	public Optional<Usuario> findByUsuario(String usuario);	
 	//findByUsuario --> findBy é palavra reservada e Usuario é o nome do atributo usuario (na model)
---
+++ blogPessoal/src/test/java/org/generation/blogPessoal/modelos/UsuarioModelTest.java
@@ -0,0 +1,14 @@
+package org.generation.blogPessoal.modelos;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+import org.junit.jupiter.api.Test;
+
+class UsuarioModelTest {
+
+	@Test
+	void test() {
+		fail("Not yet implemented");
+	}
+
+}
